fef49a505052c32fe8ddfdc9db32298b7bfc70c2
Abdulrahnam almowaled <164738163+ua1-3@users.noreply.github.com>
2025-05-14 17:14:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// This block gets the time to use in the following code
 var today = new Date();
 var hourNow = today.getHours();
-// Declaring the greeting
+
 var greeting;
-// Changing the content of the greeting message depending on
-// the time that was given by the Date() block
+
 if (hourNow <12) {
     greeting="صباح الخير";
 }
